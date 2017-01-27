1c7a43b4037c41e1f629ee0c49fc23882a9ac3fa
Mikhail Tyryshkin <tyryshkinm@gmail.com>
2017-01-27 19:33:04 +0700

    Регистрация и авторизация работают. Нужно добавить различные проверки при регистрации и входе на сайт, выводить ошибки. Модель раскидать по отдельным моделям.

--- simpleblog.my/application/models/registration_model.php
+++ simpleblog.my/application/models/registration_model.php
@@ -1,32 +1,7 @@
 <?php
-require_once("connect_db.php");
+
 class registration_model extends model
 {
-   /* public function add_user()
-    {
 
-            if (isset($_POST['register'])) {
-                $username = $_POST['username'];
-                $password = $_POST['password'];
-                $first_name = $_POST['first_name'];
-                $second_name = $_POST['second_name'];
-                header('Location: /login');
-
-            if ($password == $_POST['repeat_password'])
-            {
-                $query = "INSERT INTO users (username, password, first_name, second_name) VALUES (:username, :password, :first_name, :second_name)";
-                $add_user_bd = $connect_bd->prepare($query);
-                $add_user_bd->bindValue(':username', $username);
-                $add_user_bd->bindValue(':password', $password);
-                $add_user_bd->bindValue(':first_name', $first_name);
-                $add_user_bd->bindValue(':second_name', $second_name);
-                $result = $add_user_bd -> execute();
-                header('location: view_login.html');
-            }
-            echo "Passwords do not match";
-            header('Location: /');
-        }
-    }*/
 }
 
-?>
