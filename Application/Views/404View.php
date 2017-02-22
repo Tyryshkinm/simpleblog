df3ae8c601a1921ecdcc451137c939189408b147
Mikhail Tyryshkin <tyryshkinm@gmail.com>
2017-02-22 19:52:52 +0700

    test

---
+++ Application/Views/404View.php
@@ -0,0 +1,2 @@
+<div class="pager"><h1>Page Not Found</h1></div>
+<div class="pager"> <h1>404</h1> </div>
---
+++ Application/Views/UserRestorePassView.php
@@ -0,0 +1,8 @@
+<div class="login">
+    <p>Restore password</p>
+    <form method="post" action="/user/restore">
+        <input type="password" name="password" placeholder="new password" required /><br>
+        <input type="password" name="repeatPassword" placeholder="repeat password" required /><br>
+        <input type="submit" class="btn btn-primary" name="submit" value="Save" />
+    </form>
+</div>
