1065c40aa483347c18da6c56ec06fdbcc1595071
Mikhail Tyryshkin <tyryshkinm@gmail.com>
2017-02-22 19:55:56 +0700

    test2

--- simpleblog.my/Application/Views/404View.php
+++
@@ -1,2 +0,0 @@
-<div class="pager"><h1>Page Not Found</h1></div>
-<div class="pager"> <h1>404</h1> </div>
--- simpleblog.my/Application/Views/UserLoginView.php
+++
@@ -1,8 +0,0 @@
-<div class="login">
-    <p>Login</p>
-    <form method="post" action="/user/login">
-        <input type="text" name="username" placeholder="username" required /></br>
-        <input type="password" name="password" placeholder="password" required /></br>
-        <input type="submit" class="btn btn-primary" name="login" value="Login" />
-    </form>
-</div>
